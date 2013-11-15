617cd341cfca798bb1b429318b5c742b5c07b0f5
yoshinobu <milk@milk.local>
2013-11-16 07:16:16 +0900

    ほとんど完成版

--- js/setting.js
+++ js/setting.js
@@ -18,22 +18,22 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '2': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1': { icon: 'icons/new_gikaihi.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
   '3': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
   '4': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
-  '5': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
-  '6': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '5': { icon: 'icons/waste.svg', color: '#C75746', bcolor: '#0AB971' },
+  '6': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#4E6D00' },
   '7': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '8': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
-  '9': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '8': { icon: 'icons/forest.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '9': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#4E6D00' },
   '10': { icon: 'icons/our-streets.svg', color: '#C75746', bcolor: '#D33673' },
-  '11': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#D33673' },
-  '12': { icon: 'icons/street-lights.svg', color: '#C75746', bcolor: '#D33673' },
-  '13': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#D33673' },
-  '14': { icon: 'icons/environment.svg', color: '#C75746', bcolor: '#2A3A03' },
-  '15': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
-  '16': { icon: 'icons/wind.svg', color: '#C75746', bcolor: '#2A3A03'  }
+  '11': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#D33673' },
+  '12': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#D33673' },
+  '13': { icon: 'icons/legislative.svg', color: '#C75746', bcolor: '#D33673' },
+  '14': { icon: 'icons/environment-admin.svg', color: '#C75746', bcolor: '#2A3A03' },
+  '15': { icon: 'icons/pig.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  '16': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#2A3A03'  }
 };
 
 
